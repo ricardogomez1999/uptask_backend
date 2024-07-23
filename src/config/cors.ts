6201bf311b2c10bc9e97e526759ed224eda59925
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2024-07-23 12:51:56 -0600

    CORS problem

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,18 +1,22 @@
 import { CorsOptions } from "cors";
 
+// export const corsConfig: CorsOptions = {
+//   origin: function (origin, callback) {
+//     const whitelist = [process.env.FRONTEND_URL];
+
+//     if (process.argv[2] === "--api") {
+//       whitelist.push(undefined);
+//     }
+
+//     if (whitelist.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("CORS error"));
+//     }
+//   },
+// };
+
 export const corsConfig: CorsOptions = {
-  origin: function (origin, callback) {
-    console.log(process.argv);
-    const whitelist = [process.env.FRONTEND_URL];
-
-    if (process.argv[2] === "--api") {
-      whitelist.push(undefined);
-    }
-
-    if (whitelist.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS error"));
-    }
-  },
+  origin: "*",
+  optionsSuccessStatus: 200,
 };
